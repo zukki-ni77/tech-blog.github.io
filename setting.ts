bf375e8a434cc76d0e0db8eea2a35ef0ccb28a75
zukki-ni77 <yingtail804@gmail.com>
2026-04-30 17:38:58 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://zukki-ni77.github.io/tech-blog.github.io/";
